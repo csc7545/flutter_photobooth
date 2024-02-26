3451ba2ee88c3339a6b59e2942587788c24da476
unduck <csc7545@naver.com>
2024-02-27 01:14:15 +0900

    Add legacy external storage support and usage descriptions for photo library access*** ***Update display_picture.dart to include buttons for taking another photo, saving the photo, and sharing the photo

--- lib/screens/camera/widgets/display_picture.dart
+++ lib/screens/camera/widgets/display_picture.dart
@@ -1,10 +1,19 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'dart:typed_data';
 
 class DisplayPicture extends StatelessWidget {
   const DisplayPicture({Key? key, required this.imagePath}) : super(key: key);
 
   final String imagePath;
+
+  Future<void> saveImageToGallery(String imagePath) async {
+    final Uint8List bytes = await File(imagePath).readAsBytes();
+    final result = await ImageGallerySaver.saveImage(bytes);
+
+    print('Image saved to gallery: $result');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,11 +44,26 @@ class DisplayPicture extends StatelessWidget {
               fit: BoxFit.contain,
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Back'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Take again'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    saveImageToGallery(imagePath);
+                  },
+                  child: const Text('Save Photo'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Share Photo'),
+                ),
+              ],
             ),
           ],
         ),
